a6ae6e92e6e09018017e766f87eaadbe2443eabc
nicolas <nicolaser911@gmail.com>
2023-02-17 19:07:43 -0300

    un poquito de codigo y un poquito de estilo

---
+++ script.js
@@ -0,0 +1,17 @@
+const nombre = document.querySelector(".contenedor1__Login__nombre");
+const email = document.querySelector(".contenedor1__Login__email");
+const contraseña = document.querySelector(".contenedor1__Login__contraseña");
+const confirmar = document.querySelector(".contenedor1__Login__confContraseña");
+const campostxt = document.querySelector(".campostxt");
+const modo = document.querySelector(".campostxt");
+var tipo = 0;
+
+
+modo.addEventListener("Click",()=>{
+	if(campostxt.style.color = "#000") {
+       alert();
+       // campostxt.style.color = "#2AC176";
+    } else {
+        campostxt.style.color = "#000";
+    }
+});
